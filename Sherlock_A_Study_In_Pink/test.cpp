98b1b656609ab7e24bee07a803988673922ac2b9
Thiên Cường <169175415+nhtc2005@users.noreply.github.com>
2024-05-09 18:37:00 +0700

    File test

---
+++ Sherlock_A_Study_In_Pink/test.cpp
@@ -0,0 +1,11 @@
+#include "..."
+
+using namespace std;
+
+int main()
+{
+  StudyInPinkProgram *test = new StudyInPinkProgram("sa_tc_02_config.txt");
+  test->run(true);
+  delete test;
+  return 0;
+}
