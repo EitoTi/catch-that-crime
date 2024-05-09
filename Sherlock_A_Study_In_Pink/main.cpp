0d72fad122cc85f7333f1b2ff1bb7ea172387ca9
L1ghtChas3r <datquochoang0587@gmail.com>
2024-05-09 09:12:52 +0700

    Test case for BaseBag

--- Sherlock_A_Study_In_Pink/main.cpp
+++ Sherlock_A_Study_In_Pink/main.cpp
@@ -4,98 +4,53 @@
 #include "movingObject.h"
 #include "map.h"
 #include "arrayMovingObject.h"
+#include "BaseBag.h"
 
-void g_satc_02() { // global
-    cout << "----- Sample Testcase 02 -----" << endl;
-    Configuration* config = new Configuration("sa_tc_02_config.txt");
-    cout << config->str() << endl;
-    delete config;
-}
-
-class TestStudyInPink {
-public:
-    TestStudyInPink() = default;
-
-    static void satc_01() {
-        cout << "----- Sample Testcase 01 -----" << endl;
-        int num_walls = 3;
-        Position arr_walls[] = { Position(1, 2), Position(2, 3), Position(3, 4) };
-        int num_fake_walls = 1;
-        Position arr_fake_walls[] = { Position(2, 0) };
-
-        Map* map = new Map(10, 10, num_walls, arr_walls, num_fake_walls, arr_fake_walls);
-
-        Sherlock* sherlock = new Sherlock(1, "RUU", Position(1, 3), map, 250, 450);
-        cout << sherlock->str() << endl;
-
-        Watson* watson = new Watson(2, "LU", Position(2, 1), map, 300, 350);
-        cout << watson->str() << endl;
-
-        Criminal* criminal = new Criminal(0, Position(7, 9), map, sherlock, watson);
-        cout << criminal->str() << endl;
-
-        cout << "* Sherlock makes a move" << endl;
-        sherlock->move();
-        cout << sherlock->str() << endl;
-
-        cout << "* Watson makes a move" << endl;
-        watson->move();
-        cout << watson->str() << endl;
-
-        cout << "* Criminal makes a move" << endl;
-        criminal->move();
-        cout << criminal->str() << endl;
-
-        ArrayMovingObject* arr_mv_objs = new ArrayMovingObject(10);
-        arr_mv_objs->add(criminal);
-        arr_mv_objs->add(sherlock);
-        arr_mv_objs->add(watson);
-        cout << arr_mv_objs->str() << endl;
-    }
-
-    static void satc_02() {
-        g_satc_02();
-    }
-
-    static void satc_03() {
-        // Sample test for RobotC
-        cout << "----- Sample Testcase 03 -----" << endl;
-        int num_walls = 3;
-        Position arr_walls[] = { Position(1, 2), Position(2, 3), Position(3, 4) };
-        int num_fake_walls = 1;
-        Position arr_fake_walls[] = { Position(2, 0) };
-
-        Map* map = new Map(10, 10, num_walls, arr_walls, num_fake_walls, arr_fake_walls);
-
-        Sherlock* sherlock = new Sherlock(1, "RUU", Position(1, 3), map, 250, 450);
-        Watson* watson = new Watson(2, "LU", Position(2, 1), map, 300, 350);
-        Criminal* criminal = new Criminal(0, Position(7, 9), map, sherlock, watson);
-        criminal->move();
-
-        RobotC* robotC = new RobotC(3, Position(7, 9), map, criminal);
-        cout << "Current position of Criminal: " << criminal->str() << endl;
-        cout << "Criminal makes a move" << endl;
-        criminal->move();
-        cout << "New position of Criminal: " << criminal->str() << endl;
-
-        cout << "Current position of robotC: " << robotC->str() << endl;
-        cout << "RobotC makes a move" << endl;
-        robotC->move();
-        cout << "New position of robotC: " << robotC->str() << endl;
-
-        delete sherlock;
-        delete watson;
-        delete criminal;
-        delete map;
-        delete robotC;
-    }
-};
 
 int main()
 {
-    TestStudyInPink::satc_01();
-    TestStudyInPink::satc_02();
-    TestStudyInPink::satc_03();
+    int num_walls = 3;
+    Position arr_walls[] = { Position(1, 2), Position(2, 3), Position(3, 4) };
+    int num_fake_walls = 1;
+    Position arr_fake_walls[] = { Position(2, 0) };
+
+    Map* map = new Map(10, 10, num_walls, arr_walls, num_fake_walls, arr_fake_walls);
+
+    Sherlock* sherlock = new Sherlock(1, "RUU", Position(1, 3), map, 101, 150);
+
+    BaseItem* magicBook = new MagicBook();
+    BaseItem* energyDrink = new EnergyDrink();
+    BaseItem* firstAid = new FirstAid();
+    BaseItem* excemptionCard = new ExcemptionCard();
+    BaseItem* passingCard = new PassingCard("RobotS");
+
+    BaseBag* sherlockBag = new SherlockBag(sherlock);
+    sherlockBag->insert(magicBook);
+    sherlockBag->insert(energyDrink);
+    sherlockBag->insert(firstAid);
+    sherlockBag->insert(energyDrink);
+    sherlockBag->insert(excemptionCard);
+    sherlockBag->insert(passingCard);
+    sherlockBag->insert(firstAid);
+    sherlockBag->insert(excemptionCard);
+    cout << sherlockBag->str() << endl; // Test insert()
+    // ________________________________
+    // Test get()
+    // sherlockBag->get();
+    // cout << sherlockBag->str() << endl;
+    // sherlockBag->get();
+    // cout << sherlockBag->str() << endl;    
+    // sherlockBag->get();
+    // cout << sherlockBag->str() << endl;
+    // sherlockBag->get();
+    // cout << sherlockBag->str() << endl;
+
+    // ________________________________
+    // Test get(ItemType)
+    // sherlockBag->get(MAGIC_BOOK);
+    // cout << sherlockBag->str() << endl;
+    // sherlockBag->get(EXCEMPTION_CARD);
+    // cout << sherlockBag->str() << endl;
 
     return 0;
 }
